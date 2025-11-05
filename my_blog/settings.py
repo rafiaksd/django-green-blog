6b975bfee2bc6867359557890f030c268f4cd9e4
An0nym0usShad0w <76418474+rafiaksd@users.noreply.github.com>
2025-11-05 19:10:55 +0600

    ngrok lol

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -2,6 +2,8 @@ from pathlib import Path
 import os 
 from dotenv import load_dotenv
 
+#NGROK LETS GO
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
